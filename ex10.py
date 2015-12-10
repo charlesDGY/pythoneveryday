643f25091cf966df70e120e42cd44d36dad50c6f
dgy <edward.d.erlic@gmail.com>
2015-12-10 10:29:22 +0800

    ex14

--- ex10.py
+++ ex10.py
@@ -14,14 +14,23 @@ print persian_cat
 print backslash_cat
 print fat_cat
 
-while True:
-    for i in ["/","-","|","\\","|"]:
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s" % i,
-        print "%s\r" % i,
+for i in ["/","-","|","\\","|"]:
+    print "%s\r" % i,
+
+gang_a = "gang_a is\awhat??"
+gang_b = "gang_b is\bwhat??"
+gang_f = "gang_f is\fwhat??"
+gang_u = "gang_u is \u559c what??"
+gang_U = "gang_U is \U559c6b22 what??"
+gang_v = "gang_v is\vwhat??"
+gang_ooo = "gang_ooo is \110 what??"
+gang_xhh = "gang_xhh is \x43 what??"
+
+print gang_a
+print gang_b
+print gang_f
+print gang_u.decode("raw_unicode_escape")
+print gang_U.decode("raw_unicode_escape")
+print gang_v
+print gang_ooo
+print gang_xhh
---
+++ ex11.py
@@ -0,0 +1,23 @@
+"""
+/******************************************************************************
+ *
+ *       COPYRIGHT NOTICE
+ *       Copyright (c) 2015
+ *       All rights reserved
+ *
+ *       @author       :  Gaoyang Dai
+ *       @email        :  edward.d.erlic@gmail.com
+ *       @file         :  /home/dgy/Programming/python everyday/ex11.py
+ *       @date         :  2015/12/09 19:36
+ *       @description  :
+ *
+ *****************************************************************************/
+"""
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
